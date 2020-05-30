6eeb4e3032afd9543cbc8e3191063114c233a370
alsiendo <alsiendodewantara@gmail.com>
2020-05-30 12:14:37 +0700

    Push all the files

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function menuClick(x) {
+  x.classList.toggle("change");
+  var y = document.getElementById("displaying");
+  if (y.style.display == "block") {
+    y.style.display = "none";
+  } else {
+    y.style.display = "block";
+  }
+}
